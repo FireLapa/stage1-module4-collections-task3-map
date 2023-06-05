dfaf8b53c32b3145a7f6c4fdebe2eec80aa6e282
Lapa <lapabig@gmail.com>
2023-06-05 15:02:47 +0600

    Updated

--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -1,8 +1,24 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class KeyValueSwapper {
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
+        Map<String, Integer> swappedMap = new HashMap<>();
+
+        System.out.println(sourceMap);
+
+        for (Map.Entry<Integer, String> entry : sourceMap.entrySet()) {
+            String value = entry.getValue();
+            int key = entry.getKey();
+
+            if (swappedMap.containsKey(value) && swappedMap.get(value) < key) {
+                continue;
+            }
+            swappedMap.put(value, key);
+        }
+
+        return swappedMap;
     }
 }
--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,21 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+
+        Map<String, Integer> wordRepetitionMap = new HashMap<>();
+        String[] words = sentence.toLowerCase().split("\\W+");
+
+        for (String word : words) {
+            if (!word.isEmpty()) {
+                int count = wordRepetitionMap.getOrDefault(word, 0);
+                wordRepetitionMap.put(word, count + 1);
+            }
+        }
+
+        return wordRepetitionMap;
     }
 }
